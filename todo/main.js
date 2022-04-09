5319e065ab2983edf35c59b9058b704a5b76c913
Nicholas Bielecki <nicholas.j.bielecki@gmail.com>
2022-04-09 10:08:25 -0700

    040922 commit

--- todo/main.js
+++ todo/main.js
@@ -1,4 +1,4 @@
-let counter = 2
+
 const form = document.nameForm1
 
 axiosGet()
@@ -6,13 +6,13 @@ axiosGet()
 //submit button
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    counter ++
-
     let newApi = {
-        title: counter,
-        description: form.input1.value
-    }
-
+        title: form.input1.value,
+        description: form.input2.value,
+        price: form.input3.value,
+        imgUrl: form.input4.value,
+        
+        }
     clear()
     form.input1.value = ""
 
@@ -22,52 +22,61 @@ form.addEventListener("submit", (event) => {
 })
 
 //clear 
-function clear (){
-    while(container.firstChild){
-        container.removeChild(container.firstChild)
+    function clear (){
+        while(container.firstChild){
+            container.removeChild(container.firstChild)
+        }
     }
-}
 
 //axios get
 function axiosGet(){
     clear()
+
     axios.get("https://api.vschool.io/nicholasBielecki/todo")
     .then(responce => {
         let newData = responce
         console.log(newData)
     for(i=0;i<responce.data.length; i++){
         //consts and lets
-        const newli = document.createElement("li")
-        const newInput = document.createElement("input")
-        const container = document.getElementById("container")
-        const deleteButton = document.createElement("input")
-        let ivar = i
+            const newli = document.createElement("li")
+            const newPic = document.createElement("img")
+            const newInput = document.createElement("input")
+            const container = document.getElementById("container")
+            const deleteButton = document.createElement("input")
+            const {description,title,price,imgUrl} = newData.data[i] 
+            let ivar = i
         //checkbox stuff
-        newInput.setAttribute("type", "button")
-        newInput.value = "completed"
-        newInput.addEventListener("click", ()=>{
-            axios.put(`https://api.vschool.io/nicholasBielecki/todo/${newData.data[ivar]._id}`, {completed: !newData.data[ivar].completed})
-            .then(axiosGet())
-            .catch(error=>error)
-        })
+            newInput.setAttribute("type", "checkbox")
+            if(responce.data[i].completed === true){
+                newli.setAttribute("style", "text-decoration: line-through")
+                newInput.checked  = true
+            }
+            newInput.addEventListener("click", ()=>{
+                axios.put(`https://api.vschool.io/nicholasBielecki/todo/${newData.data[ivar]._id}`, {completed: !newData.data[ivar].completed})
+                .then(axiosGet())
+                .catch(error=>console.log(error))
+            })
         //delete stuff
-        deleteButton.setAttribute("type", "button")
-        deleteButton.value = "delete"
-        deleteButton.addEventListener("click", ()=>{
-            axios.delete(`https://api.vschool.io/nicholasBielecki/todo/${newData.data[ivar]._id}`)
-            .then(axiosGet)
-            .catch(error=>console.log(error))
-        })
+            deleteButton.setAttribute("type", "button")
+            deleteButton.value = "delete"
+            deleteButton.addEventListener("click", ()=>{
+                axios.delete(`https://api.vschool.io/nicholasBielecki/todo/${newData.data[ivar]._id}`)
+                .then(axiosGet())
+                .catch(error=>console.log(error))
+            })  
         
 
-if(responce.data[i].completed === false){
-    newli.setAttribute("style", "text-decoration: line-through")}
-
-        newli.textContent= responce.data[i].description
+        //text and image content   
+                newli.textContent= `Title: ${title} Description: ${description} Price: ${price} Url: ${imgUrl}`
+                newPic.src = `${newData.data[ivar].imgUrl}`
+                newPic.setAttribute("class", "imageClass")
+                
         //append
-        container.appendChild(newli)
-        newli.appendChild(newInput)
-        newli.appendChild(deleteButton)
+            container.appendChild(newli)
+            newli.appendChild(newInput)
+            newli.appendChild(deleteButton)
+            newli.appendChild(editButton)
+            newli.appendChild(newPic)
 
         
         
@@ -75,4 +84,6 @@ if(responce.data[i].completed === false){
     }})
     .catch(error => console.log(error))
 }
+
+
 
